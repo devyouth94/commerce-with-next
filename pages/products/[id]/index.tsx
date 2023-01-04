d272d38ebb63fbb8e72f09df2d73bd6ff938dfa2
devyouth94 <devyouth94@gmail.com>
2023-01-05 00:00:01 +0900

    Feat: 상세페이지 기본 템플릿 구현

--- pages/products/[id]/index.tsx
+++ pages/products/[id]/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from 'next/legacy/image';
 import Carousel from 'nuka-carousel/lib/carousel';
 import { useState } from 'react';
@@ -22,6 +23,23 @@ const Products = () => {
 
   return (
     <>
+      <Head>
+        <meta
+          property="og:url"
+          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+        />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="When Great Minds Don’t Think Alike" />
+        <meta
+          property="og:description"
+          content="How much does culture influence creative thinking?"
+        />
+        <meta
+          property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+        />
+      </Head>
+
       <Carousel autoplay withoutControls wrapAround slideIndex={index}>
         {images.map((image) => (
           <Image
@@ -34,6 +52,7 @@ const Products = () => {
           />
         ))}
       </Carousel>
+
       <div>
         {images.map((image, idx) => (
           <Image
